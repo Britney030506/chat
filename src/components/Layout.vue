1e113906223c95432d7f0f61deb47c6330356674
Britney <britneypalma16@gmail.com>
2023-05-12 12:39:02 -0600

    se agrega el proyecto

---
+++ src/components/Layout.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ChatInfo from "@/modules/chatinfo/ChatInfo.vue";
+</script>
+
+<template>
+  <h1>Layout</h1>
+  <ChatInfo />
+</template>
+
+<style lang="css" scoped></style>
